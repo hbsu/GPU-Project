94ef8c1bdf2989f455e8dec7c6165aa3b8f666c8
hbsu <122388917+hbsu@users.noreply.github.com>
2025-05-10 19:42:21 -0400

    Update cpuStructural.v

--- CPUV2/cpuStructural.v
+++ CPUV2/cpuStructural.v
@@ -37,18 +37,18 @@ module sext9 (input wire[8:0] in9, output wire[15:0] out16);
     assign out16 = {{8{in9[8]}}, in9};
 endmodule
 
-module mux3b (
-    input wire [2:0] in1,
-    input wire [2:0] in2,
-    input wire control,
-    output wire [2:0] out
-);
+//module mux3b (
+//    input wire [2:0] in1,
+//    input wire [2:0] in2,
+//    input wire control,
+//    output wire [2:0] out
+//);
 
-    mux1b m0 (.a(in1[0]), .b(in2[0]), .sel(control), .y(out[0]));
-    mux1b m1 (.a(in1[1]), .b(in2[1]), .sel(control), .y(out[1]));
-    mux1b m2 (.a(in1[2]), .b(in2[2]), .sel(control), .y(out[2]));
+//    mux1b m0 (.a(in1[0]), .b(in2[0]), .sel(control), .y(out[0]));
+//    mux1b m1 (.a(in1[1]), .b(in2[1]), .sel(control), .y(out[1]));
+//    mux1b m2 (.a(in1[2]), .b(in2[2]), .sel(control), .y(out[2]));
 
-endmodule
+//endmodule
 
 
 module mux1b (
@@ -66,6 +66,19 @@ module mux1b (
     or  (y, a_and, b_and);
 endmodule
 
+module mux16b41(
+	input wire[15:0] in1, in2, in3 ,in4,
+	input wire[1:0] control, 
+	output wire[15:0] out
+);
+	wire[15:0] mux_out1, mux_out2;
+	mux16b out1(.in1(in1), .in2(in2), .control(control[0]), .out(mux_out1)); 
+	mux16b out2(.in1(in3), .in2(in4), .control(control[0]), .out(mux_out2));
+	mux16b out3(.in1(mux_out1), .in2(mux_out2), .control(control[1]), .out(out));
+
+
+
+endmodule
 
 module mux16b (
     input wire [15:0] in1,
@@ -106,8 +119,8 @@ module dff(
 endmodule
 
 module register(
-	input[15:0] in,
-	input clock, enable, reset,
+	input wire [15:0] in,
+	input wire clock, enable, reset,
 	output reg [15:0] out
 );
 	dff dfr0  (clock, enable, in[0],  reset, out[0]);
@@ -130,21 +143,58 @@ module register(
 endmodule
 
 
-module registerFile( //one clock cycle delay
-	input wire [2:0] writeReg,
-	input wire [2:0] readReg1,
-	input wire [2:0] readReg2,
-	input wire [15:0] writeData,
-	input wire clock, regWriteEnable, reset,
-	output wire[15:0] readData1, readData2
-);
-	//8 general purpose 16-bit registers
-	reg[15:0] registers[7:0];
-	register regA (registers[readReg1], clock, 1'b1, reset, readData1); //output readData1
-	register regB (registers[readReg2], clock, 1'b1, reset, readData2); //output readData2
-	//Next line will only occur if writeEnable is high. 
-	register inReg(writeData, clock, regWriteEnable, reset, registers[writeReg]); //write into associated register
+//module registerFile( //one clock cycle delay
+//	input [2:0] writeReg,
+//	input wire [2:0] readReg1,
+//	input wire [2:0] readReg2,
+//	input wire [15:0] writeData,
+//	input wire clock, regWriteEnable, reset,
+//	output wire[15:0] readData1, readData2
+//);
+//	//8 general purpose 16-bit registers
+//	reg[15:0] registers[7:0];
+//	register regA (registers[readReg1], clock, 1'b1, reset, readData1); //output readData1
+//	register regB (registers[readReg2], clock, 1'b1, reset, readData2); //output readData2
+//	//Next line will only occur if writeEnable is high. 
+//	register inReg(writeData, clock, regWriteEnable, reset, registers[writeReg]); //write into associated register
 	
+//endmodule
+
+module registerFile (
+    input wire [2:0] writeReg,
+    input wire [2:0] readReg1,
+    input wire [2:0] readReg2,
+    input wire [15:0] writeData,
+    input wire clock, regWriteEnable, reset,
+    output wire [15:0] readData1, readData2
+);
+    // Declare individual registers using your `register` module
+    wire [15:0] reg_out [7:0];
+
+    // 8 general purpose 16-bit registers
+    reg [15:0] registers [7:0];
+
+    // Asynchronous Read (Combinational)
+    assign readData1 = registers[readReg1];
+    assign readData2 = registers[readReg2];
+
+    // Synchronous Write
+    always @(posedge clock or posedge reset) begin
+  
+        if(reset) begin
+            registers[0] <= 16'b0;
+            registers[1] <= 16'b0;
+            registers[2] <= 16'b0;
+            registers[3] <= 16'b0;
+            registers[4] <= 16'b0;
+            registers[5] <= 16'b0;
+            registers[6] <= 16'b0;
+            registers[7] <= 16'b0;
+        end
+        else if (regWriteEnable) begin
+            registers[writeReg] <= writeData;
+        end
+    end
 endmodule
 
 `define ADD     4'b0001
@@ -170,21 +220,73 @@ module alu(
 	input wire [15:0] in1, //either R1 or PC
 	input wire [15:0] in2, //either R2 or imm
 	input wire [3:0] aluFunc,
-	output wire [15:0] aluResult
+	output reg [15:0] aluResult
 ); 
 
-	begin 
+	always @(*)begin 
 		case(aluFunc)
-			`ADD: 	assign aluResult = in1 + in2;
-			`SUB: 	assign aluResult = in1 - in2;
-			`MUL: 	assign aluResult = in1 * in2;
-			`AND: 	assign aluResult = in1 & in2;
-			`NOT: 	assign aluResult = ~in1;
-			`ST: 	assign aluResult = in1 + in2; //pc + offset
-			`LD: 	assign aluResult = in1 + in2; 
-			`STR: 	assign aluResult = in1 + in2; //pc + incomming Register Data
-			`LDR: 	assign aluResult = in1 + in2;
-			default: assign aluResult = 16'bZ;
+			`ADD: 	 aluResult = in1 + in2;
+			`SUB: 	 aluResult = in1 - in2;
+			`MUL: 	 aluResult = in1 * in2;
+			`AND: 	 aluResult = in1 & in2;
+			`NOT: 	 aluResult = ~in1;
+			`ST: 	 aluResult = in1 + in2; //pc + offset
+			`LD: 	 aluResult = in1 + in2; 
+			`STR: 	 aluResult = in1 + in2; //pc + incomming Register Data
+			`LDR: 	 aluResult = in1 + in2;
+			default:  aluResult = 16'bZ;
         endcase 
 	end
+endmodule
+
+`define ADD     4'b0001
+`define SUB     4'b0010
+`define MUL     4'b0011
+`define AND     4'b0100
+//operations
+`define NOT     4'b0101
+`define ST      4'b0110
+`define LD      4'b0111
+`define STR     4'b1000
+`define LDR     4'b1001
+`define STI     4'b1010 //Unimplemented
+`define LDI     4'b1011 //Unimplemented
+`define JMP     4'b1100
+`define RET     4'b1101
+`define BRZ     4'b1110
+`define BRN     4'b1111
+`define NOP     4'b0000
+
+//register read decoder
+module regReadDec(
+	input wire [15:0] IF_ID_Inst,
+	output reg[2:0] RFreadReg1, RFreadReg2
+);
+	always @(*) begin
+		if(IF_ID_Inst[15:12] == `ADD || IF_ID_Inst[15:12] == `SUB || IF_ID_Inst[15:12] == `MUL || 
+		IF_ID_Inst[15:12] == `AND || IF_ID_Inst[15:12] == `NOT) begin
+			RFreadReg1 <= IF_ID_Inst[7:5];
+			RFreadReg2 <= IF_ID_Inst[4:2];
+		end else if(IF_ID_Inst[15:12] == `ST)begin
+			RFreadReg1 <= IF_ID_Inst[11:9]; //where to pull value
+		end else if(IF_ID_Inst[15:12] == `STR) begin
+			RFreadReg1 <= IF_ID_Inst[11:9]; //where to pull value
+			RFreadReg2 <= IF_ID_Inst[8:6]; //Where to read following address
+		end else if(IF_ID_Inst[15:12] == `LDR) begin
+			RFreadReg2 <= IF_ID_Inst[8:6]; //Where to read following address
+		end else if(IF_ID_Inst[15:12] == `JMP 
+					|| IF_ID_Inst[15:12] == `BRZ 
+					|| IF_ID_Inst[15:12] == `BRN) begin
+			RFreadReg1 <= IF_ID_Inst[11:9]; //where to read the register from
+		end else if(IF_ID_Inst[15:12] == `RET) begin 
+			RFreadReg1 <= 3'b0111; //read from r7
+		end else begin
+			RFreadReg1 <= IF_ID_Inst[11:9]; 	//TEMPORARY DEFAULT
+			RFreadReg2 <= IF_ID_Inst[11:9];		//TEMPORARY DEFAULT
+		end
+	
+	end
+
+
+
 endmodule
